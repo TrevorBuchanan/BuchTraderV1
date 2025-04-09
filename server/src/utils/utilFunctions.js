56a5fcffeff316bb0dda75f6aa72536a9fe1f1a5
Trevor Buchanan <buchanan14.trevor@gmail.com>
2025-04-09 10:32:02 -0700

    Util function fixes

--- server/src/utils/utilFunctions.js
+++ server/src/utils/utilFunctions.js
@@ -3,26 +3,33 @@ function lerp(a, b, t) {
     return a + t * (b - a)
 }
 
-function upTrendForLength(series, length) { // TODO: FIX -> Make general purpose
-    if (series.length < length) return false;
-    for (let i = series.length - length; i < series.length - 1; i++) {
-        if (series[i]['price'] >= series[i + 1]['price']) {
+function upTrendForLength(series, length) {
+    greaterForLen = length + 1;
+    if (series.length < greaterForLen) return false;
+
+    const startIndex = series.length - greaterForLen;
+    const endIndex = series.length - 1;
+    for (let i = startIndex; i < endIndex; i++) {
+        if (series[i] >= series[i + 1]) {
             return false;
         }
     }
     return true;
 }
 
-function downTrendForLength(series, length) {  // TODO: FIX -> Make general purpose
-    if (series.length < length) return false;
-    for (let i = series.length - length; i < series.length - 1; i++) {
-        if (series[i]['price'] <= series[i + 1]['price']) {
+function downTrendForLength(series, length) {
+    lesserForLen = length + 1;
+    if (series.length < lesserForLen) return false;
+
+    const startIndex = series.length - lesserForLen;
+    const endIndex = series.length - 1;
+    for (let i = startIndex; i < endIndex; i++) {
+        if (series[i] <= series[i + 1]) {
             return false;
         }
     }
     return true;
 }
-
 
 module.exports = {
     lerp,
